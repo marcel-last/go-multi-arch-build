5a6c4a601962cb6b4d15e47062e6a8cd2383b3bd
Marcel Last <100458278+marcel-last@users.noreply.github.com>
2022-08-28 23:24:29 +0930

    Initial commit

---
+++ scripts/compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Author: Marcel Last
+# Date: 2022-08-28
+# Descritpion: This bash script compiles and builds Go binaries for "common"..
+# operating systems and platform architecture combinations.
+
+set -euxo pipefail
+
+GOOS=(windows darwin linux)
+GOARCH=(386 amd64 arm64)
+MODULE_SOURCE=$MODULE_NAME.go
+
+for os in "${GOOS[@]}"; do
+	for arch in "${GOARCH[@]}"; do
+        binary_name=$MODULE_NAME-v$MODULE_VERSION-$arch
+        if [ $os == "windows" ]; then
+            GOOS=$os GOARCH=$arch go build -o bin/$os/$binary_name.exe $MODULE_SOURCE
+        # Go version 1.15 drops support for 32-bit binaries; darwin/386 and darwin/arm (https://go.dev/doc/go1.15)
+        # Skip this operating system/architecture combination.
+        elif [ $os == "darwin" ] && [ $arch == "386" ]; then 
+            continue
+        else
+		    GOOS=$os GOARCH=$arch go build -o bin/$os/$binary_name $MODULE_SOURCE
+        fi
+	done
+done
